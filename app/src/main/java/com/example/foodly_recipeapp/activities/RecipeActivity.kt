ebd0908fa196b751f0a6fd81ac571d959fc27a2f
Daniel <daniel090901@gmail.com>
2020-08-13 21:21:35 +0300

    add recipe card view, recipe activity.

---
+++ app/src/main/java/com/example/foodly_recipeapp/activities/RecipeActivity.kt
@@ -0,0 +1,12 @@
+package com.example.foodly_recipeapp.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.foodly_recipeapp.R
+
+class RecipeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recipe)
+    }
+}
