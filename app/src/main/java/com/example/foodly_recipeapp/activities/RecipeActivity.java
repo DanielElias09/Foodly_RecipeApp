eeb269b958dfd7b1988c6c5e3c6f8eb29af53909
Daniel <daniel090901@gmail.com>
2020-08-13 22:28:46 +0300

    add recipe activity

---
+++ app/src/main/java/com/example/foodly_recipeapp/activities/RecipeActivity.java
@@ -0,0 +1,14 @@
+package com.example.foodly_recipeapp.activities;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.foodly_recipeapp.R;
+
+public class RecipeActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe);
+    }
+}
